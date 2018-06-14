5b9e31a15a61c2151aee10dcdefffd13b7a118d5
Md Abu Zafar <mdabuzafar78@gmail.com>
2018-06-14 15:34:18 +0530

    Import table formatter

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,4 +1,5 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
   @default_count 4
 
   @moduledoc """
@@ -24,6 +25,8 @@ defmodule Issues.CLI do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
     |> sort_into_ascending_order
+    |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
