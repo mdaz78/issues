cea5a3ce0d26f542288c4ee18f32d371430ecd7e
Md Abu Zafar <mdabuzafar78@gmail.com>
2018-06-14 15:26:17 +0530

    Sort issues

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -20,9 +20,10 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
+    |> sort_into_ascending_order
   end
 
   def decode_response({:ok, body}), do: body
@@ -31,6 +32,10 @@ defmodule Issues.CLI do
     {_, message} = List.keyfind(error, "message", 0)
     IO.puts "Error fetching from github: #{message}"
     System.halt(2)
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort(list_of_issues, fn(i1, i2) -> Map.get(i1, "created_at") <= Map.get(i2, "created_at") end)
   end
 
   @doc """
