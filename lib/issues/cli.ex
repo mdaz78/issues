5340110b4aefd3e6f13801d533773c697d1c1848
Md Abu Zafar <mdabuzafar78@gmail.com>
2018-06-14 14:50:41 +0530

    Handle possible errors with Issues.GithubIssues.fetch

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -22,6 +22,15 @@ defmodule Issues.CLI do
 
   def process({user, project, _count}) do
     Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from github: #{message}"
+    System.halt(2)
   end
 
   @doc """
